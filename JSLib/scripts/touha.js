eb6fa050e473ff5f261f007ad0f431389183d803
javaDevp <1129597198@qq.com>
2016-08-31 15:19:29 +0800

    添加项目文件。

---
+++ JSLib/scripts/touha.js
@@ -0,0 +1,19 @@
+﻿+(function (window, undefined) {
+    //记录全局中的touha对象
+    var _touha = window.touha,
+        //为了直接通过touha()进行实例化，不需要new touha()
+        touha = function () {
+            return new touha.fn.init();
+        }
+
+    touha.fn = touha.prototype = {
+        init: function () {
+            return this;
+        }
+    };
+
+    //让touha()返回的对象，可以访问touha对象的属性
+    touha.fn.init.prototype = touha.fn;
+
+    window.touha = touha;
+})(window, undefined);
